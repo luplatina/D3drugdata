24aea82462ae5f809f96116d77ab276fcf1dea38
luplatina <luplatina@gmail.com>
2017-03-19 13:54:28 -0400

    Solve the ghp problem

---
+++ makerelease.sh
@@ -0,0 +1,6 @@
+#if If you no longer wish to use Git LFS
+#rm .git/hooks/pre-push
+cp ADdrug.json bubble_datavisual.html release
+ghp-import -p release/
+
+echo find at https://luplatina.github.io/D3drugdata/bubble_datavisual.html
